45a7a7da4f17ccf15cb54154a815721517bf43c7
selebrak <3260516+selebrak@users.noreply.github.com>
2024-12-06 17:41:50 -0600

    Refactor for simplicity

--- lib/aoc_2024/day02.ex
+++ lib/aoc_2024/day02.ex
@@ -15,14 +15,14 @@ defmodule Aoc2024.Day02 do
   end
 
   def part2() do
-    @input_filepath
-    |> File.stream!()
-    |> Stream.map(&String.trim/1)
-    |> Stream.map(&String.split/1)
-    |> Stream.map(fn report -> Enum.map(report, &String.to_integer/1) end)
-    |> Task.async_stream(&dampened_test_safe/1)
-    |> Stream.map(fn {:ok, result} -> result end)
-    |> Enum.sum()
+    # @input_filepath
+    # |> File.stream!()
+    # |> Stream.map(&String.trim/1)
+    # |> Stream.map(&String.split/1)
+    # |> Stream.map(fn report -> Enum.map(report, &String.to_integer/1) end)
+    # |> Task.async_stream(&dampened_test_safe/1)
+    # |> Stream.map(fn {:ok, result} -> result end)
+    # |> Enum.sum()
   end
 
   def count_safe_reports(filepath) do
@@ -32,92 +32,90 @@ defmodule Aoc2024.Day02 do
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.split/1)
     |> Stream.map(fn report -> Enum.map(report, &String.to_integer/1) end)
-    |> Task.async_stream(&test_safe/1)
+    |> Task.async_stream(&report_safe?/1)
     |> Stream.map(fn {:ok, result} -> result end)
     |> Enum.sum()
   end
 
-  # TODO: refactor logic and optimize for tail recursion?
-  def test_safe([e1, e2 | tail]) do
-    level_difference = e1 - e2
+  def report_safe?([e1, e2 | tail]) do
+    diff = e1 - e2
     cond do
-      -1 >= level_difference and level_difference >= -3 ->
-        test_safe_incr([e2 | tail])
-      1 <= level_difference and level_difference <= 3 ->
-        test_safe_decr([e2 | tail])
+      diff in -1..-3//-1 ->
+        safe_up?([e2 | tail])
+      diff in 1..3//1 ->
+        safe_down?([e2 | tail])
       true ->
         0
     end
   end
 
-  def test_safe_incr([_e1]), do: 1
+  def safe_up?([_e1]), do: 1
 
-  def test_safe_incr([e1, e2 | tail]) do
-    level_difference = e1 - e2
+  def safe_up?([e1, e2 | tail]) do
+    diff = e1 - e2
     cond do
-      -1 >= level_difference and level_difference >= -3 ->
-        test_safe_incr([e2 | tail])
+      diff in -1..-3//-1 ->
+        safe_up?([e2 | tail])
       true ->
         0
     end
   end
 
+  def safe_down?([_e1]), do: 1
 
-  def test_safe_decr([_e1]), do: 1
-
-  def test_safe_decr([e1, e2 | tail]) do
-    level_difference = e1 - e2
+  def safe_down?([e1, e2 | tail]) do
+    diff = e1 - e2
     cond do
-      1 <= level_difference and level_difference <= 3 ->
-        test_safe_decr([e2 | tail])
+      diff in 1..3//1 ->
+        safe_down?([e2 | tail])
       true ->
         0
     end
   end
 
-  def dampened_test_safe([e1, e2, e3 | tail]) do
-    level_difference = e1 - e2
-    cond do
-      -1 >= level_difference and level_difference >= -3 ->
-        dampened_test_safe_incr([e2, e3 | tail])
-      1 <= level_difference and level_difference <= 3 ->
-        dampened_test_safe_decr([e2, e3 | tail])
-      -1 >= (e1 - e3) and (e1 - e3) >= -3 ->
-        dampened_test_safe_incr([e2, e3 | tail])
-      1 <= (e1 - e3) and (e1 - e3) > 3 ->
-        dampened_test_safe_decr([e1, e3 | tail])
-      true ->
-        test_safe([e1, e3 | tail])
-    end
-  end
+  # def dampened_test_safe([e1, e2, e3 | tail]) do
+  #   level_difference = e1 - e2
+  #   cond do
+  #     -1 >= level_difference and level_difference >= -3 ->
+  #       dampened_test_safe_incr([e2, e3 | tail])
+  #     1 <= level_difference and level_difference <= 3 ->
+  #       dampened_test_safe_decr([e2, e3 | tail])
+  #     -1 >= (e1 - e3) and (e1 - e3) >= -3 ->
+  #       dampened_test_safe_incr([e2, e3 | tail])
+  #     1 <= (e1 - e3) and (e1 - e3) > 3 ->
+  #       dampened_test_safe_decr([e1, e3 | tail])
+  #     true ->
+  #       test_safe([e1, e3 | tail])
+  #   end
+  # end
 
 
-  def dampened_test_safe_incr([_e1]), do: 1
+  # def dampened_test_safe_incr([_e1]), do: 1
 
-  def dampened_test_safe_incr([e1, e2 | tail]) do
-    level_difference = e1 - e2
-    cond do
-      -1 >= level_difference and level_difference >= -3 ->
-        dampened_test_safe_incr([e2 | tail])
-      level_difference > 0 -> # shift down
-        test_safe_incr([e2 | tail])
-      level_difference <= 0 -> # jump up or plateau
-        test_safe_incr([e1 | tail])
-    end
-  end
+  # def dampened_test_safe_incr([e1, e2 | tail]) do
+  #   level_difference = e1 - e2
+  #   cond do
+  #     -1 >= level_difference and level_difference >= -3 ->
+  #       dampened_test_safe_incr([e2 | tail])
+  #     level_difference > 0 -> # shift down
+  #       test_safe_incr([e2 | tail])
+  #     level_difference <= 0 -> # jump up or plateau
+  #       test_safe_incr([e1 | tail])
+  #   end
+  # end
 
 
-  def dampened_test_safe_decr([_e1]), do: 1
+  # def dampened_test_safe_decr([_e1]), do: 1
 
-  def dampened_test_safe_decr([e1, e2 | tail]) do
-    level_difference = e1 - e2
-    cond do
-      1 <= level_difference and level_difference <= 3 ->
-        dampened_test_safe_decr([e2 | tail])
-      level_difference <= 0 -> # shift up
-        test_safe_decr([e2 | tail])
-      level_difference > 0 -> # jump down or plateau
-        test_safe_decr([e1 | tail])
-    end
-  end
+  # def dampened_test_safe_decr([e1, e2 | tail]) do
+  #   level_difference = e1 - e2
+  #   cond do
+  #     1 <= level_difference and level_difference <= 3 ->
+  #       dampened_test_safe_decr([e2 | tail])
+  #     level_difference <= 0 -> # shift up
+  #       test_safe_decr([e2 | tail])
+  #     level_difference > 0 -> # jump down or plateau
+  #       test_safe_decr([e1 | tail])
+  #   end
+  # end
 end
