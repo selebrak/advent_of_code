aa5e6cad1737b08fd20c3444252716dc541753a0
selebrak <3260516+selebrak@users.noreply.github.com>
2024-12-05 20:33:21 -0600

    Solve 2024 Day 01
    - Also lay out initial project structure.
    
    Squashed commit of the following:
    
    commit b285118431abe1801ecfb0d1a5d3b5bbeece7650
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 20:31:13 2024 -0600
    
        Update README.md
    
    commit c678ad80298133ae0c642bcef5fb5655bc9cc9f4
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 20:29:50 2024 -0600
    
        Refactor to support multiple AoC years
    
    commit e26b8a31fbd44df62145e931497709865e64d0dd
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 19:52:20 2024 -0600
    
        Move inputs into priv
    
    commit 6ae876a76e0a24278c3669f02ce41317f768f741
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 19:48:32 2024 -0600
    
        Refactor input handling
    
    commit 8c6736bbfbf957649e8a505d468225b03d4da7e6
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 18:49:54 2024 -0600
    
        Add tests
    
    commit 40eb015886429b2b5b0898ee7a10379c6c6272cc
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Thu Dec 5 18:13:15 2024 -0600
    
        Add day 01 star 2 solution
    
    commit aa2ed98aeabc5ea487dc794c54ea1454e5d264dd
    Author: selebrak <3260516+selebrak@users.noreply.github.com>
    Date:   Wed Dec 4 22:02:24 2024 -0600
    
        Initial Day 01 commit

---
+++ lib/aoc_2024/day01.ex
@@ -0,0 +1,60 @@
+defmodule Aoc2024.Day01 do
+  @moduledoc """
+  Advent of Code Day 1 2024
+  """
+
+  @base_inputs_path Path.expand("../../priv/inputs/2024", __DIR__)
+  @day01_input_path Path.join(@base_inputs_path, "day-01-input.txt")
+
+  def print_solutions() do
+    IO.puts(part1())
+    IO.puts(part2())
+  end
+
+  def part1() do
+    {list1, list2} = lists_from_file(@day01_input_path)
+    inter_list_distance(list1, list2)
+  end
+
+  def part2() do
+    {list1, list2} = lists_from_file(@day01_input_path)
+    similarity_score(list1, list2)
+  end
+
+  def inter_list_distance(list1, list2) do
+    sorted_list1 = Enum.sort(list1)
+    sorted_list2 = Enum.sort(list2)
+
+    sorted_list1
+    |> Enum.zip(sorted_list2)
+    |> Enum.map(fn {x,y} -> abs(x - y) end)
+    |> Enum.reduce(0, &+/2) # or Enum.sum()
+  end
+
+  def similarity_score(list1, list2) do
+    list2_freqmap = Enum.frequencies(list2)
+
+    list1
+    |> Enum.map(&multiply_by_frequency(&1, list2_freqmap))
+    |> Enum.sum()
+  end
+
+  def multiply_by_frequency(input, freqmap) do
+    freqmap
+    |> Map.get(input, 0)
+    |> Kernel.*(input)
+  end
+
+  def lists_from_file(filename) do
+    filename
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+    end)
+    |> Enum.map(fn [x, y] -> {x, y} end)
+    |> Enum.unzip()
+  end
+end
