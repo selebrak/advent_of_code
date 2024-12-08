f71b49a41ec660f75ace5acaa46906a1cc045bf8
selebrak <3260516+selebrak@users.noreply.github.com>
2024-12-08 12:13:28 -0600

    Initial commit

---
+++ lib/aoc_2024/day04.ex
@@ -0,0 +1,21 @@
+defmodule Aoc2024.Day04 do
+  @moduledoc """
+  Advent of Code Day 4 2024
+  """
+
+  @input_filepath Path.expand("../../priv/inputs/2024/day-04-input.txt", __DIR__)
+
+  def print_solutions() do
+    IO.inspect(part1())
+    IO.inspect(part2())
+  end
+
+  def part1() do
+  end
+
+  def part2() do
+  end
+
+  def read_file(filepath) do
+  end
+end
---
+++ test/aoc_2024/day04_test.exs
@@ -0,0 +1,16 @@
+defmodule Aoc2024.Day04Test do
+  use ExUnit.Case
+  #TODO doctest Aoc2024.Day04
+
+  import Aoc2024.Day04
+
+  #TODO add tests for individual functions
+
+  test "Day 04 Star 1" do
+    assert part1() == nil
+  end
+
+  test "Day 04 Star 2" do
+    assert part2() == nil
+  end
+end
